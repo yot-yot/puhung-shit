2ee610a009c67e888043e035d95ce9738f303e41
yotyot <82359786+yot-yot@users.noreply.github.com>
2025-05-02 00:03:33 +0900

    json

--- src/module/haribo.js
+++ src/module/haribo.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 
 let dataJson = fs.readFileSync("../../storage/input/data.json");
-
+/*
 for(i=0;i<100;i++){
-    console.log('')
+    console.log(`{"bagNum":${i},"white":${parseInt(Math.random()*9)},"red":${parseInt(Math.random()*9)}, "green":${parseInt(Math.random()*9)}, "yellow":${parseInt(Math.random()*9)},"orange":${parseInt(Math.random()*9)}},`);
 }
+*/
+
+let nowSee = "";
