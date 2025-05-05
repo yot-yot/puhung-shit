c47d458578723fb36a931cc88796648cc9e223da
yotyot <82359786+yot-yot@users.noreply.github.com>
2025-05-05 22:59:44 +0900

    fuck

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const haribo = require("./module/haribo");
 
 const server = app.listen(80, () => {
     console.log('Start server : localhost');
 });
 
-app.get('/api/user/:data', async (req, res) => { // url path, async (요청, 응답)
+app.get('/:data', async (req, res) => { // url path, async (요청, 응답)
+    let start = performance.now();
     let {data} = req.params;
-    res.send(data); // 성공 시 보여지는 응답
+    haribo.sibal(100000);
+    let end = performance.now();
+
+    res.send(`${data} 색이 가장 많은 봉지 : `+haribo.colorMost(data)+` 실행 시간 : ${end-start}ms`); // 성공 시 보여지는 응답
 })
--- src/module/haribo.js
+++ src/module/haribo.js
@@ -1,10 +1,84 @@
 const fs = require("fs");
 
-let dataJson = fs.readFileSync("../../storage/input/data.json");
+let dataJson = JSON.parse(fs.readFileSync("./storage/input/data.json"));
+let mrJson = JSON.parse(fs.readFileSync("./storage/output/memory.json"));
+
+
+
+let colorKind = ["orange", "red", "green", "yellow", "white"];
+let nowSee; //tq
 /*
-for(i=0;i<100;i++){
-    console.log(`{"bagNum":${i},"white":${parseInt(Math.random()*9)},"red":${parseInt(Math.random()*9)}, "green":${parseInt(Math.random()*9)}, "yellow":${parseInt(Math.random()*9)},"orange":${parseInt(Math.random()*9)}},`);
+{
+    "longMr" : [
+        {
+            "name":"",
+            "mkDate":"",
+            "content":""
+        }
+    ],
+    "shortMr" : [
+        {
+            "name":"",
+            "mkDate":"",
+            "content":""
+        }
+    ]
 }
 */
 
-let nowSee = "";
+
+function sibal(n) {
+    var text = "";
+    for (i = 0; i < n; i++) {
+        text += (`{"bagNum":${i},"white":${parseInt(Math.random() * 9)},"red":${parseInt(Math.random() * 9)},"green":${parseInt(Math.random() * 9)},"yellow":${parseInt(Math.random() * 9)},"orange":${parseInt(Math.random() * 9)}},  `);
+    }
+    text = text.slice(0, -1);
+    text=text.split(",  ");
+    text[n-1]=text[n-1].replace(", ","");
+    //text = text.splice(n, n-1)
+    
+    var json = {
+        "bag": {
+            "colorNum": []
+        }
+    }
+    for(i=0;i<n;i++){
+        json.bag.colorNum.push(JSON.parse(text[i]));
+    }
+    //console.log(json);
+    fs.writeFileSync("./storage/input/data.json", JSON.stringify(json), "utf-8");
+}
+
+function haribo(i, data) {
+    var json = {
+        "name": `bag${i}`,
+        "mkDate": new Date().toString(),
+        "content": dataJson.bag.colorNum[i]
+    };
+    mrJson.shortMr.push(json);
+    fs.writeFileSync("./storage/output/memory.json", JSON.stringify(mrJson), "utf-8");
+    return 0;
+}
+
+//haribo(0, nowSee);
+
+function colorMost(color, n) {
+    dataJson = JSON.parse(fs.readFileSync("./storage/input/data.json"));
+    var re = [];
+    console.log(eval(`dataJson.bag.colorNum[0].${color}`))
+    var d;
+    for (i = 0; i < dataJson.bag.colorNum.length; i++) {
+        d = dataJson.bag.colorNum[i];
+        if (Math.max(d.orange, d.red, d.yellow, d.green, d.white) == eval(`d.${color}`)) {
+            re.push(dataJson.bag.colorNum[i].bagNum);
+        }
+    }
+    if (re == []) return -1;
+    return re;
+}
+
+module.exports = {
+    haribo,
+    colorMost,
+    sibal
+}
