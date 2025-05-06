fefe3512739a7bc7d14828a281b3459015a684b0
yotyot <82359786+yot-yot@users.noreply.github.com>
2025-05-06 22:42:02 +0900

    ㅅㅂ

--- src/index.js
+++ src/index.js
@@ -2,15 +2,41 @@ const express = require('express');
 const app = express();
 const haribo = require("./module/haribo");
 
+const ban = 10000; //반복횟수
+
 const server = app.listen(80, () => {
     console.log('Start server : localhost');
 });
 
 app.get('/:data', async (req, res) => { // url path, async (요청, 응답)
     let start = performance.now();
-    let {data} = req.params;
-    haribo.sibal(100000);
+    let { data } = req.params;
+    haribo.sibal(ban); //데이터 수정
     let end = performance.now();
-
-    res.send(`${data} 색이 가장 많은 봉지 : `+haribo.colorMost(data)+` 실행 시간 : ${end-start}ms`); // 성공 시 보여지는 응답
+    var sib = haribo.colorMost(data);
+    haribo.haribo(); //단기 기억 머시깽이
+    var ddd = haribo.long(data);
+    
+    console.log(ddd)
+    var fuck = [];
+    for(i=0;i<ddd.length;i++){
+        fuck.push(ddd[i].bagNum)
+    }
+    var s = countCommonElements(fuck, sib)
+    console.log(fuck.length, sib.length, s)
+    res.send(`${data} 색이 가장 많은 봉지 : ` + sib + ` 실행 시간 : ${end - start}ms    `+fuck); // 성공 시 보여지는 응답
 })
+
+function countCommonElements(arr1, arr2) {
+    const set1 = new Set(arr1);
+    const set2 = new Set(arr2);
+    let count = 0;
+
+    for (const item of set1) {
+        if (set2.has(item)) {
+            count++;
+        }
+    }
+
+    return count;
+}
--- src/module/haribo.js
+++ src/module/haribo.js
@@ -2,11 +2,10 @@ const fs = require("fs");
 
 let dataJson = JSON.parse(fs.readFileSync("./storage/input/data.json"));
 let mrJson = JSON.parse(fs.readFileSync("./storage/output/memory.json"));
+let dmJson = JSON.parse(fs.readFileSync("./storage/output/dummy.json"));
 
 
 
-let colorKind = ["orange", "red", "green", "yellow", "white"];
-let nowSee; //tq
 /*
 {
     "longMr" : [
@@ -30,7 +29,13 @@ let nowSee; //tq
 function sibal(n) {
     var text = "";
     for (i = 0; i < n; i++) {
-        text += (`{"bagNum":${i},"white":${parseInt(Math.random() * 9)},"red":${parseInt(Math.random() * 9)},"green":${parseInt(Math.random() * 9)},"yellow":${parseInt(Math.random() * 9)},"orange":${parseInt(Math.random() * 9)}},  `);
+        text += (`{"bagNum":${i},
+            "white":${parseInt(Math.random() * 9)},
+            "red":${parseInt(Math.random() * 9)},
+            "green":${parseInt(Math.random() * 9)},
+            "yellow":${parseInt(Math.random() * 9)},
+            "orange":${parseInt(Math.random() * 9)}}
+            ,  `);
     }
     text = text.slice(0, -1);
     text=text.split(",  ");
@@ -49,36 +54,80 @@ function sibal(n) {
     fs.writeFileSync("./storage/input/data.json", JSON.stringify(json), "utf-8");
 }
 
-function haribo(i, data) {
-    var json = {
-        "name": `bag${i}`,
-        "mkDate": new Date().toString(),
-        "content": dataJson.bag.colorNum[i]
-    };
-    mrJson.shortMr.push(json);
+function haribo() {
+    dataJson = JSON.parse(fs.readFileSync("./storage/input/data.json"));
+    mrJson = JSON.parse(fs.readFileSync("./storage/output/memory.json"));
+    mrJson.shortMr.push(dataJson.bag.colorNum);
     fs.writeFileSync("./storage/output/memory.json", JSON.stringify(mrJson), "utf-8");
     return 0;
 }
 
+function long(color){
+    //dataJson = JSON.parse(fs.readFileSync("./storage/input/data.json"));
+    j=0
+    mrJson = JSON.parse(fs.readFileSync("./storage/output/memory.json"));
+    mrJson.longMr = [];
+    for (i = 0; i < mrJson.shortMr[0].length; i++) {
+        
+        d = mrJson.shortMr[0][i];
+        //console.log(d)
+        //console.log(eval(`d.${color}`))
+        if (eval(`d.${color}`)>=4) {
+            
+            mrJson.longMr.push(d);
+            j++
+        }else{
+            dmJson.noNeed.push(d);
+        }
+    }
+    //console.log(j, mrJson.longMr.length)
+    fs.writeFileSync("./storage/output/memory.json", JSON.stringify(mrJson), "utf-8");
+    fs.writeFileSync("./storage/output/dummy.json", JSON.stringify(dmJson), "utf-8");
+    return mrJson.longMr;
+}
 //haribo(0, nowSee);
 
-function colorMost(color, n) {
+function colorMost(color) {
     dataJson = JSON.parse(fs.readFileSync("./storage/input/data.json"));
     var re = [];
-    console.log(eval(`dataJson.bag.colorNum[0].${color}`))
+    //console.log(eval(`dataJson.bag.colorNum[0].${color}`))
     var d;
+    var num = 0;
     for (i = 0; i < dataJson.bag.colorNum.length; i++) {
         d = dataJson.bag.colorNum[i];
         if (Math.max(d.orange, d.red, d.yellow, d.green, d.white) == eval(`d.${color}`)) {
             re.push(dataJson.bag.colorNum[i].bagNum);
+            num+=eval(`d.${color}`);
         }
     }
+    //console.log(num/n);
     if (re == []) return -1;
+    return re;
+}
+
+function colorMost2(color) {
+    dataJson = JSON.parse(fs.readFileSync("./storage/output/memory.json"));
+    var re = [];
+    //console.log(eval(`dataJson.bag.colorNum[0].${color}`))
+    var d;
+    var num = 0;
+    for (i = 0; i < dataJson.longMr.length; i++) {
+        d = dataJson.longMr[i];
+        if (Math.max(d.orange, d.red, d.yellow, d.green, d.white) == eval(`d.${color}`)) {
+            re.push(dataJson.longMr[i].bagNum);
+            num+=eval(`d.${color}`);
+        }
+    }
+    //console.log(num/n);
+    if (re == []) return -1;
+    //console.log(re)
     return re;
 }
 
 module.exports = {
     haribo,
     colorMost,
-    sibal
+    colorMost2,
+    sibal,
+    long
 }
