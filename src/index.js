a01a3c1371529bec69e817b9b23c13c650500b96
yotyot <82359786+yot-yot@users.noreply.github.com>
2025-05-27 20:08:00 +0900

    no fuck

--- src/index.js
+++ src/index.js
@@ -9,25 +9,28 @@ const server = app.listen(80, () => {
 });
 
 app.get('/:data', async (req, res) => { 
+
     let start = performance.now();
     let { data } = req.params;
-    haribo.sibal(ban); //데이터 수정
+    haribo.writeBase(ban); //데이터 수정
     let end = performance.now();
-    var sib = haribo.colorMost(data);
-    haribo.haribo(); //단기 기억 머시깽이
+
+    var mostColor = haribo.colorMost(data);
+    haribo.haribo(); //단기 기억 저장?
     
-    var ddd = haribo.long(data);
+    var longMr = haribo.long(data);//장기 기억인듯
     
-    //console.log(ddd)
-    var fuck = [];
+    var result = [];
+
     let start2 = performance.now();
-    for(i=0;i<ddd.length;i++){
-        fuck.push(ddd[i].bagNum);
+    for(i=0;i<longMr.length;i++){
+        result.push(longMr[i].bagNum);
     }
     let end2 = performance.now();
-    var s = countCommonElements(fuck, sib);
-    console.log(fuck.length, sib.length, s, );
-    res.send(`${data} / 일반 : ` +  ` 실행 시간 : ${end - start}ms, ${data} / 뇌 모방 : `+` 실행 시간2 : ${end2 - start2}ms,`+"정확도:"+s/sib.length*100+"%"); // 성공 시 보여지는 응답
+
+    var goodRt = countCommonElements(result, mostColor);
+    console.log(result.length, mostColor.length, goodRt);
+    res.send(`${data} / 일반 : ` +  ` 실행 시간 : ${end - start}ms, ${data} / 뇌 모방 : `+` 실행 시간2 : ${end2 - start2}ms,`+"정확도:"+goodRt/mostColor.length*100+"%"); // 성공 시 보여지는 응답
 })
 
 function countCommonElements(arr1, arr2) {
